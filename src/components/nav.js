d365a1d8208ed0a68848198ad33d2ac9317a3ce4
KacperGnys <kacpergnys2002@gmail.com>
2024-01-26 22:49:19 +0000

    Home Navbar and Project Navbar - first upload
    Created both the Navbar for the Home page and project page using the Figma designs as a template. Also created the message dropdown however that is not finalised yet. Javascript needed to make some things like usernames and profile pictures dynamic still needed.
    
    I have added an envelope icon for the message part of my Navbar. I have also added 2 random pictures to serve as "Users" so I could test out the positioning of my profile pictures, both in the navbar, and in the message dropdown.
    
    I pray this looks the same on everyone else's machines as it does on mine.

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,57 +1,66 @@
 
-import { Navbar, Container, Nav, Form, NavDropdown, Button } from 'react-bootstrap';
+import './Nav.css'
+import NotificationBox from './NotificationBox.js';
 import logoImg from '../sources/Co_Create_Logo_blue.png';
+import msgImg from '../sources/Msg_Icon.png';
+import profileImg from '../sources/Darwizzy.jpg';
+import userImg1 from '../sources/ProfileImg1.jpg';
+
+
 
 const OurNav = () => { //Lambda style of return, is more compact and cleaner
-        
 
-    const navbarStyle = {
-        backgroundColor: '#7CC0CB', // Replace with your desired color code
-      };
-    
     return (
-        <>
-        <Navbar style={navbarStyle}>
-            <Container className="d-inline-block">
-              <Navbar.Brand href="/">
-              <img
-                    alt="LOGO"
-                    src={logoImg}
-                    width="60"
-                    height="60"
-                    className="d-inline-block"
-                  />{' '}
-              </Navbar.Brand>
-                <Nav>
-                  <Nav.Link href="#home">
-                  <img
-                    alt="USERNAME IMG"
-                    src="#"
-                    width="30"
-                    height="30"
-                    className="d-inline-block"
-                  />{' '}
-                  Username</Nav.Link>
-                  <img
-                    alt="MSG"
-                    src="#"
-                    width="30"
-                    height="30"
-                    className="d-inline-block"
-                  />{' '}
+      
+            <div className="Outside">
+              <div>
+                <ul>
+                  <li><a href="#home"><img src={logoImg} width="50" height="50"></img></a></li>
+                  <li>
+                    <a href="#home">
+                      <img className="ProfilePic" src={profileImg} width="47" height="47"></img>
+                    </a>
+                  </li>
+                  <li><a href="#home"><div className="Username">Username</div></a></li>
+                </ul>
+              </div>
+              <div>
                 
-                </Nav>
-                  <Form>
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                </Form>
-            </Container>
-          </Navbar>
-        </>      
+              </div>
+              <div>
+                <ul>
+                  <li>
+                    <form className="SearchBar">
+                      <input type="search" placeholder="Search" aria-label="Search"></input>
+                    </form>
+                  </li>
+                  <li>
+                  <div class="dropdown">
+                    <button class="DropBtn">
+                      <img src={msgImg} alt="CoCreate" width="50" height="50"></img>
+                    </button>
+                    <div class="dropdown-menu MsgGrid dropdown-menu-end">
+                      <div>
+                        <h2 class="DropdownHeader">
+                          Messages
+                          <span class="AddSymbols">+ &#x1F4DE; &#128249;</span>
+                        </h2>
+                        <ul class="DropdownLinks">
+                        <li class="Message"><a href="#"><img className="ProfilePic" src={userImg1} width="50" height="50"></img></a></li>
+                        <li class="Message"><a href="#"><img className="ProfilePic" src={userImg1} width="50" height="50"></img></a></li>
+                        <li class="Message"><a href="#"><img className="ProfilePic" src={userImg1} width="50" height="50"></img></a></li>
+                        <li class="Message"><a href="#"><img className="ProfilePic" src={userImg1} width="50" height="50"></img></a></li>
+                        <li class="Message"><a href="#"><img className="ProfilePic" src={userImg1} width="50" height="50"></img></a></li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  </li>
+                  <li className="NotificationBox"><NotificationBox num = {5}/></li>
+                </ul>
+              </div>
+            </div>
+
         );
     };
 
