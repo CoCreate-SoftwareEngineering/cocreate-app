12cb21667052540275a078d5c4a87bc4c01f0a45
caerayoung <cly2000@hw.ac.uk>
2024-01-27 22:45:55 +0000

    Updated login

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,12 +8,14 @@ const Login = () => { //Lambda style of return, is more compact and cleaner
                 <img src={logoImg} class="logo"></img>
             </div>
             <div class="right-container">
-                <p>Login</p><br/>
+                <h1>Login</h1><br/>
                 <label>Username</label><br/>
                 <input type="text" placeholder='joebloggs123'/><br/>
                 <label>Password</label><br/>
                 <input type="password" placeholder='*********'/><br/>
+                <p>Forgotten password?</p>
                 <button type='submit'>Submit</button>
+                <p>New to CoCreate? Register</p>  
             </div>
         </div>
     );
