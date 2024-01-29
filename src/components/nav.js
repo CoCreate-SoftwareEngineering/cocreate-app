63fe19f76e1c81584739bec3f0baeccd15402668
rohanveit <rohan.veit@gmail.com>
2024-01-29 11:21:30 +0000

    Fixing navbar refs and button background animation

--- src/components/nav.js
+++ src/components/nav.js
@@ -15,13 +15,13 @@ const OurNav = () => { //Lambda style of return, is more compact and cleaner
             <div className="Outside">
               <div>
                 <ul>
-                  <li><a href="#home"><img src={logoImg} width="50" height="50"></img></a></li>
+                  <li><a href="/"><img src={logoImg} width="50" height="50"></img></a></li>
                   <li>
-                    <a href="#home">
+                    <a href="/">
                       <img className="ProfilePic" src={profileImg} width="47" height="47"></img>
                     </a>
                   </li>
-                  <li><a href="#home"><div className="Username">Username</div></a></li>
+                  <li><a href="/"><div className="Username">Username</div></a></li>
                 </ul>
               </div>
               <div>
