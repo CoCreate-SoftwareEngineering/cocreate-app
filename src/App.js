e489d706f662f29d0e7c514dac8135705f0db0fe
rohanveit <rohan.veit@gmail.com>
2024-01-31 13:22:47 +0000

    routing changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import Home from './components/Home'
 import Login from './components/Login';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Switch , Routes, Route } from 'react-router-dom'
 import Project from './components/Project';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path = "/" element={<Login/>}></Route> {/* When you would like to add a new page, add it here, path = the part after main domain for URL of page and element is the page element to redirect to */}
-        <Route path = "/home" element={<Home/>}></Route>
-        <Route path = "/project" element={<Project/>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <HashRouter>
+        <Switch>
+          <Routes>
+            <Route exact path = "/" element={<Login/>}></Route> {/* When you would like to add a new page, add it here, path = the part after main domain for URL of page and element is the page element to redirect to */}
+            <Route path = "/home" element={<Home/>}></Route>
+            <Route path = "/project" element={<Project/>}></Route>
+          </Routes>
+      </Switch>
+    </HashRouter>   
   );
 }
 
