e81aaff4b8fdf6395fde2f228cf1d20f260039c1
BigBirdsThigh <zb2007@hw.ac.uk>
2024-02-29 13:56:51 +0000

    toolbar tweak

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react';
 import './Project.css'
 
-const IconButton = ( {image, alt, clicked} ) => {
+
+const IconButton = ( {alt, clicked, imagePath} ) => {
+    const [imageSrc, setImageSrc] = useState(null)
+
+    useEffect(() => {
+        import(`../sources/button-images/${imagePath}.png`).then(module => {
+            setImageSrc(module.default);
+        })
+        .catch(error => {
+            console.error(`Image not found for alt: ${alt}`)
+        })
+
+    })
+
+    if(!imageSrc) {
+        return<div>Loading...</div> // placeholder if image not found or loaded
+    }
+
     return (
         <div className = "icon-button-container">
             <button className = "icon-button" onClick = {clicked}>
-                <img src = {image} alt = {alt}/>
+                <img src = {imageSrc} alt = {alt}/>
             </button>
             <div className = "icon-button-label">{alt}</div>
         </div>
